627143d24e7dc86bd538f202dc524c4d32634bdf
Nelson Aranda <nelson.aranda@itti.digital>
2024-07-13 13:10:54 -0400

    change to postgresql

---
+++ prisma/migrations/20240713164757_postgres/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
